76e02b8051e2fd0d707ad32deff80d6396620ce8
VAKARAMOKO <vakaramokokamate93@gmail.com>
2020-02-20 09:44:08 +0000

    ajax comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery3
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require jquery_ujs
 //= require summernote/summernote-bs4.min
